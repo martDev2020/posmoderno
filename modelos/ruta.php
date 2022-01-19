3d720e6df638ea4ef13d1c1ba9ee06e417ddbfe8
unknown <al221310188@gmail.com>
2022-01-19 10:40:13 -0600

    Configuración de carpetas

---
+++ modelos/ruta.php
@@ -0,0 +1,14 @@
+<?php
+class Ruta
+{
+
+    /*=============================================
+	RUTA LADO DEL CLIENTE
+	=============================================*/
+
+    static public function ctrRuta()
+    {
+        // El SSL se aplica cuando se ha configurado el acceso con las apis.
+        return "http://localhost/posmoderno/";
+    }
+}
